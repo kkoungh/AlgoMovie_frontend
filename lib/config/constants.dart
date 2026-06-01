0b2161f1cf9761cbfca615f35139a9fcfa18f265
seohee <tjdudtjgml1234@enaver.com>
2026-06-01 16:28:39 +0900

    크롬 디버깅을 위해 백엔드 기본 주소를 localhost로 수정

---
+++ lib/config/constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  // PC 크롬 브라우저 및 iOS 시뮬레이터: localhost / Android 에뮬레이터 테스트 시에는 10.0.2.2 사용
+  static const String baseUrl = 'http://localhost:3000/api';
+  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+}
